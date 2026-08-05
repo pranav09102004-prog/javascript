0b8595e503765f85bc6685518f392b7876ee52ff
Pranav Mali <pranav09102004@gmail.com>
2026-08-05 06:51:42 +0000

    javascript

---
+++ 01basic/one.js
@@ -0,0 +1,47 @@
+//if
+
+// const isuserlogin = true
+// const temp = 51
+
+// if(temp < 50 ){
+//     console.log("less than 50");
+// } else{
+//     console.log("Temprature is greter than 50")
+
+// }    
+
+// console.log("temp")
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power:${power}`)
+// }
+
+
+const balance = 200
+
+// if(balance > 500) console.log("test");
+// console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750 ){
+//     console.log("less than 750")
+// }else {
+//     console.log("Less than 1200 ")
+// }
+
+const userlogin = true
+const debitcard = true
+const loginfromgoogle = true
+
+
+if(userlogin && debitcard && 2==3) {
+    console.log("Allow to buy course")
+}
+
+if(loginfromgoogle || userlogin){
+    console.log("user looged in")
+}
